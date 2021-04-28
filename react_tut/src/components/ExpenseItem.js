83cb0730272fd7e0b3457c4af2195f5aae3fe129
epiczain1234 <mukatizain@gmail.com>
2021-04-28 16:28:03 -0400

    Further in

--- react_tut/src/components/ExpenseItem.js
+++ react_tut/src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
 import './ExpenseItem.css'
-function ExpenseItem(){
+
+function ExpenseItem(props){
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day:'2-digit'});
+    const year = props.date.getFullYear();
+    const title = props.title;
+    const price = props.amount;
     return (
         <div className="expense-item">
-            <div>3/28/2021</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
-               <h2 className="">Car Insurance</h2> 
-               <div className="expense-item__price">$500</div>
+               <h2>{title}</h2> 
+               <div className="expense-item__price">${price}</div>
             </div>
         </div>
     )
