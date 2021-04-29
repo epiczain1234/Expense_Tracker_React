65b1f74285e83b8e71e17b119213093e2addd3e8
epiczain1234 <mukatizain@gmail.com>
2021-04-29 15:56:30 -0400

    Created Seperate component for list of expenses

---
+++ react_tut/src/components/Expenses.js
@@ -0,0 +1,17 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem'
+
+function Expenses(props){
+    const expenses = props.expenses;
+    return (
+        <div className='expenses'>
+          <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date = {expenses[0].date}> </ExpenseItem>
+          <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date = {expenses[1].date}> </ExpenseItem>
+          <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date = {expenses[2].date}> </ExpenseItem>
+          <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date = {expenses[3].date}> </ExpenseItem>
+        </div>
+    )
+
+}
+
+export default Expenses;
