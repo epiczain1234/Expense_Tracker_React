68d1ad2c40eb12ce442def8e24b144ac5d743017
epiczain1234 <mukatizain@gmail.com>
2021-05-09 10:47:23 -0400

    Added multiple states for each parameter

--- react_tut/src/components/NewExpense/ExpenseForm.js
+++ react_tut/src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,37 @@
 import './ExpenseForm.css';
+import React, {useState} from 'react';
 const ExpenseForm = () => {
+    // intial state can be represented by an empty string
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
     // get the current value with every keystroke
-    const titleChangeHandler = () => {
-        console.log('title change');
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+    // get triggered when amount is chaned on page
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
     }
 
     return (
         <form>
-            <div className="new-exprense__controls"> 
-                <div className="new-exprense__control">
+            <div className="new-expense__controls"> 
+                <div className="new-expense__control">
                     <label> Title </label>
                     <input type="text" onChange={titleChangeHandler}></input>
                 </div>
-                <div className="new-exprense__control">
+                <div className="new-expense__control">
                     <label> Amount </label>
-                    <input type="number"></input>
+                    <input type="number" onChange={amountChangeHandler}></input>
                 </div>
-                <div className="new-exprense__control">
-                    <label> Amount </label>
-                    <input type="date"></input>
+                <div className="new-expense__control">
+                    <label> Date</label>
+                    <input type="date" onChange={dateChangeHandler}></input>
                 </div>
             </div>
             <div>
