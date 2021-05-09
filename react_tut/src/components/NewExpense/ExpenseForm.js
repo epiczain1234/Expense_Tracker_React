2cdcd67405bdc9c72ef639e03e174fe15111b22b
epiczain1234 <mukatizain@gmail.com>
2021-05-08 22:24:38 -0400

    Added onChangeHandler

--- react_tut/src/components/NewExpense/ExpenseForm.js
+++ react_tut/src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,16 @@
 import './ExpenseForm.css';
 const ExpenseForm = () => {
+    // get the current value with every keystroke
+    const titleChangeHandler = () => {
+        console.log('title change');
+    }
+
     return (
         <form>
             <div className="new-exprense__controls"> 
                 <div className="new-exprense__control">
                     <label> Title </label>
-                    <input type="text"></input>
+                    <input type="text" onChange={titleChangeHandler}></input>
                 </div>
                 <div className="new-exprense__control">
                     <label> Amount </label>
