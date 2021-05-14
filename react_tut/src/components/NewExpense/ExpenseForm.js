0dd77243cc085e4c6ee83f20b1ac783a7b6e51f8
epiczain1234 <mukatizain@gmail.com>
2021-05-13 22:40:07 -0400

    added

--- react_tut/src/components/NewExpense/ExpenseForm.js
+++ react_tut/src/components/NewExpense/ExpenseForm.js
@@ -5,37 +5,76 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData ={
+            title: enteredTitle,
+            amount: enteredAmount,
+            date : new Date(enteredDate)
+
+        };
+        console.log(expenseData);
+        setEnteredAmount('');
+        setEnteredDate('');
+        setEnteredTitle('');
+    };
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle : '',
+    //     enteredAmount: '',
+    //     enteredDate:''
+    // });
 
     // get the current value with every keystroke
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
+        // this react below simply schedules an update while the new function  below immedialtley executes the update, which is essential sinnce schedfuling an update can lead 
+        // to a stale state being used to create the new state
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // })
+        setEnteredTitle((prevState) =>{
+            return {...prevState, enteredTitle:event.target.value}
+        })
     }
     // get triggered when amount is chaned on page
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // })
+        setEnteredAmount((prevState) =>{
+            return {...prevState, enteredAmount:event.target.value}
+        })
     }
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput, // copies over previous data to the new state, overrides previous entered date value
+        //     enteredDate: event.target.value
+        // })
+        setEnteredDate((prevState) =>{
+            return {...prevState, enteredDate:event.target.value}
+        })
     }
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls"> 
                 <div className="new-expense__control">
                     <label> Title </label>
-                    <input type="text" onChange={titleChangeHandler}></input>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}></input>
                 </div>
                 <div className="new-expense__control">
                     <label> Amount </label>
-                    <input type="number" onChange={amountChangeHandler}></input>
+                    <input type="number" value={enteredAmount} onChange={amountChangeHandler}></input>
                 </div>
                 <div className="new-expense__control">
                     <label> Date</label>
-                    <input type="date" onChange={dateChangeHandler}></input>
+                    <input type="date" value={enteredDate} onChange={dateChangeHandler}></input>
                 </div>
             </div>
             <div>
-                <button>Add Expense</button>
+                <button type='submit'>Add Expense</button>
             </div>
         </form>
     )
